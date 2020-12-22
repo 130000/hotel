3d3d02c18e08970c2ad4c391a088555316ec0698
chenjinyang <chenjinyang>
2020-12-22 14:52:50 +0800

    add new files

---
+++ DAL/DBHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+  public class DBHelper
+    {
+        //public static SqlConnection sqlconn;
+
+        //public static void conn() {
+        
+        private static string str = "Data Source =.; Initial Catalog = HotelManage; User Id = sa; Password=abc-123";
+        //sqlconn = new SqlConnection(sql);
+        //sqlconn.Open();
+        //}
+
+        public static DataTable Query(string sql) {
+          SqlConnection conn = new SqlConnection(str);
+          SqlCommand comm = new SqlCommand(sql, conn);
+          SqlDataAdapter da = new SqlDataAdapter(comm);
+          DataTable dt = new DataTable();
+          da.Fill(dt);
+          conn.Close();
+          return dt;
+  
+      }
+
+      public static int Modify(string sql) {
+          SqlConnection conn = new SqlConnection(str);
+          conn.Open();
+          SqlCommand comm = new SqlCommand(sql, conn);
+          int res = comm.ExecuteNonQuery();
+          conn.Close();
+          return res;
+  
+      }
+
+
+
+    }
+}
